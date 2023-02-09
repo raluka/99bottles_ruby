f6ecdf31cdbe1bf2f3383db995a1edb350fd0318
Raluca Badoi <raluca.badoi@shopify.com>
2023-02-09 10:39:42 +0100

    try to implement before reading

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,119 +1,59 @@
-class CountdownSong
-  attr_reader :verse_template, :max, :min
+# frozen_string_literal: true
 
-  def initialize(verse_template:, max: 999999, min: 0)
-    @verse_template = verse_template
-    @max, @min = max, min
+class Bottles
+  def verse(line, high = 99)
+    first_rime(line) +
+      second_rime(line) +
+      third_rime(line) +
+      forth_rime(line - 1, high)
+  end
+
+  def verses(high, low)
+    high.downto(low).collect do |item|
+      verse(item)
+    end.join("\n")
   end
 
   def song
-    verses(max, min)
+    verses(99, 0)
+  end
+  private
+
+  def first_rime(line)
+    return 'No more bottles of beer on the wall, ' if line.zero?
+
+    "#{line} #{support(line)} of beer on the wall, "
   end
 
-  def verses(upper, lower)
-    upper.downto(lower).collect {|i| verse(i)}.join("\n")
+  def second_rime(line)
+    return "no more bottles of beer.\n" if line.zero?
+
+    "#{line} #{support(line)} of beer.\n"
   end
 
-  def verse(number)
-    verse_template.lyrics(number)
-  end
-end
+  def third_rime(quantity)
+    return 'Go to the store and buy some more, ' if quantity.zero?
 
-
-class BottleVerse
-  def self.lyrics(number)
-    new(BottleNumber.for(number)).lyrics
+    to_remove = 'it' if quantity == 1
+    to_remove = 'one' if quantity > 1
+    "Take #{to_remove} down and pass it around, "
   end
 
-  attr_reader :bottle_number
+  def forth_rime(quantity, high)
+    return "#{high} bottles of beer on the wall.\n" if quantity.negative?
 
-  def initialize(bottle_number)
-    @bottle_number = bottle_number
+    "#{remained_quantity(quantity)} #{support(quantity)} of beer on the wall.\n"
   end
 
-  def lyrics
-    "#{bottle_number} of beer on the wall, ".capitalize +
-    "#{bottle_number} of beer.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of beer on the wall.\n"
-  end
-end
-
-
-class BottleNumber
-  def self.for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    when 6
-      BottleNumber6
-    else
-      BottleNumber
-    end.new(number)
+  def support(quantity)
+    support = 'bottle'
+    support += 's' if quantity != 1
+    support
   end
 
-  attr_reader :number
-  def initialize(number)
-    @number = number
-  end
+  def remained_quantity(quantity)
+    return 'no more' if quantity.zero?
 
-  def to_s
-    "#{quantity} #{container}"
-  end
-
-  def quantity
-    number.to_s
-  end
-
-  def container
-    "bottles"
-  end
-
-  def action
-    "Take #{pronoun} down and pass it around"
-  end
-
-  def pronoun
-    "one"
-  end
-
-  def successor
-    BottleNumber.for(number - 1)
-  end
-end
-
-class BottleNumber0 < BottleNumber
-  def quantity
-    "no more"
-  end
-
-  def action
-    "Go to the store and buy some more"
-  end
-
-  def successor
-    BottleNumber.for(99)
-  end
-end
-
-class BottleNumber1 < BottleNumber
-  def container
-    "bottle"
-  end
-
-  def pronoun
-    "it"
-  end
-end
-
-class BottleNumber6 < BottleNumber
-  def quantity
-    "1"
-  end
-
-  def container
-    "six-pack"
+    quantity
   end
 end
