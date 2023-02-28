66fb19ff9f1a3f1490749913d7e704e1fb063831
Raluca Badoi <raluca.badoi@shopify.com>
2023-02-28 19:23:51 +0100

    Down to last verse refactor. Finding hidden concepts

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,14 +10,14 @@ class Bottles
   def verse(number)
     case number
     when 0
-      'No more bottles of beer on the wall, ' +
-        "no more bottles of beer.\n" +
-        'Go to the store and buy some more, ' +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+        "#{quantity(number)} #{container(number)} of beer.\n" +
+        "#{action(number)}" +
         "99 bottles of beer on the wall.\n"
     else
-      "#{number} #{container(number)} of beer on the wall, " +
-        "#{number} #{container(number)} of beer.\n" +
-        "Take #{pronoun(number)} down and pass it around, " +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+        "#{quantity(number)} #{container(number)} of beer.\n" +
+        "#{action(number)}" +
         "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
@@ -42,7 +42,15 @@ class Bottles
     if number == 0
       'no more'
     else
-      number
+      number.to_s
+    end
+  end
+
+  def action(number)
+    if number == 0
+      'Go to the store and buy some more, '
+    else
+      "Take #{pronoun(number)} down and pass it around, "
     end
   end
 end
