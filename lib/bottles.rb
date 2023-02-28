8f0e7ede123d85741021eccd953874566f112e4c
Raluca Badoi <raluca.badoi@shopify.com>
2023-02-28 18:10:34 +0100

    Refactor current code to be opened to changes - the flocking rule

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,12 @@
 class Bottles
+  def song
+    verses(99, 0)
+  end
+
+  def verses(upper, lower)
+    upper.downto(lower).collect { |number| verse(number) }.join("\n")
+  end
+
   def verse(number)
     case number
     when 0
@@ -11,34 +19,19 @@ class Bottles
         "1 bottle of beer.\n" +
         'Take it down and pass it around, ' +
         "no more bottles of beer on the wall.\n"
-    when 2
-      '2 bottles of beer on the wall, ' +
-        "2 bottles of beer.\n" +
-        'Take one down and pass it around, ' +
-        "1 bottle of beer on the wall.\n"
-    when 6
-      '1 six-pack of beer on the wall, ' +
-        "1 six-pack of beer.\n" +
-        'Take one down and pass it around, ' +
-        "5 bottles of beer on the wall.\n"
-    when 7
-      '7 bottles of beer on the wall, ' +
-        "7 bottles of beer.\n" +
-        'Take one down and pass it around, ' +
-        "1 six-pack of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
         "#{number} bottles of beer.\n" +
         'Take one down and pass it around, ' +
-        "#{number - 1} bottles of beer on the wall.\n"
+        "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
-  def verses(upper, lower)
-    upper.downto(lower).collect { |number| verse(number) }.join("\n")
-  end
-
-  def song
-    verses(99, 0)
+  def container(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
+    end
   end
 end
