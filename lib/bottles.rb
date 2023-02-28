39385c919c2fc3b25394f4b255cf6446466196e8
Raluca Badoi <raluca.badoi@shopify.com>
2023-02-28 14:31:59 +0100

    New request to output 1 sis-pack where it currently says 6 bottles

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -16,6 +16,16 @@ class Bottles
         "2 bottles of beer.\n" +
         'Take one down and pass it around, ' +
         "1 bottle of beer on the wall.\n"
+    when 6
+      '1 six-pack of beer on the wall, ' +
+        "1 six-pack of beer.\n" +
+        'Take one down and pass it around, ' +
+        "5 bottles of beer on the wall.\n"
+    when 7
+      '7 bottles of beer on the wall, ' +
+        "7 bottles of beer.\n" +
+        'Take one down and pass it around, ' +
+        "1 six-pack of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, " +
         "#{number} bottles of beer.\n" +
@@ -25,7 +35,7 @@ class Bottles
   end
 
   def verses(upper, lower)
-    upper.downto(lower).collect { |number| verse(number)}.join("\n")
+    upper.downto(lower).collect { |number| verse(number) }.join("\n")
   end
 
   def song
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -55,34 +55,34 @@ class BottlesTest < Minitest::Test
 
   def test_a_couple_verses
     expected =
-      "99 bottles of beer on the wall, " +
-        "99 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "98 bottles of beer on the wall.\n" +
-        "\n" +
-        "98 bottles of beer on the wall, " +
-        "98 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "97 bottles of beer on the wall.\n"
+      '99 bottles of beer on the wall, ' +
+      "99 bottles of beer.\n" +
+      'Take one down and pass it around, ' +
+      "98 bottles of beer on the wall.\n" +
+      "\n" +
+      '98 bottles of beer on the wall, ' +
+      "98 bottles of beer.\n" +
+      'Take one down and pass it around, ' +
+      "97 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verses(99, 98)
   end
 
   def test_a_few_verses
     expected =
-      "2 bottles of beer on the wall, " +
-        "2 bottles of beer.\n" +
-        "Take one down and pass it around, " +
-        "1 bottle of beer on the wall.\n" +
-        "\n" +
-        "1 bottle of beer on the wall, " +
-        "1 bottle of beer.\n" +
-        "Take it down and pass it around, " +
-        "no more bottles of beer on the wall.\n" +
-        "\n" +
-        "No more bottles of beer on the wall, " +
-        "no more bottles of beer.\n" +
-        "Go to the store and buy some more, " +
-        "99 bottles of beer on the wall.\n"
+      '2 bottles of beer on the wall, ' +
+      "2 bottles of beer.\n" +
+      'Take one down and pass it around, ' +
+      "1 bottle of beer on the wall.\n" +
+      "\n" +
+      '1 bottle of beer on the wall, ' +
+      "1 bottle of beer.\n" +
+      'Take it down and pass it around, ' +
+      "no more bottles of beer on the wall.\n" +
+      "\n" +
+      'No more bottles of beer on the wall, ' +
+      "no more bottles of beer.\n" +
+      'Go to the store and buy some more, ' +
+      "99 bottles of beer on the wall.\n"
     assert_equal expected, Bottles.new.verses(2, 0)
   end
 
@@ -135,7 +135,7 @@ class BottlesTest < Minitest::Test
 
       84 bottles of beer on the wall, 84 bottles of beer.
       Take one down and pass it around, 83 bottles of beer on the wall.
-      
+
       83 bottles of beer on the wall, 83 bottles of beer.
       Take one down and pass it around, 82 bottles of beer on the wall.
 
