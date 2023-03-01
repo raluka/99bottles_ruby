f5cafe0fece08b9f08ddb8d58fab079759a3c3ff
Raluca Badoi <raluca.badoi@shopify.com>
2023-03-01 10:43:01 +0100

    Reduce verse statement to a single template of small consistent abstractions

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,18 +8,10 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-        "#{quantity(number)} #{container(number)} of beer.\n" +
-        "#{action(number)}" +
-        "99 bottles of beer on the wall.\n"
-    else
-      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-        "#{quantity(number)} #{container(number)} of beer.\n" +
-        "#{action(number)}" +
-        "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
-    end
+    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
+      "#{action(number)}" +
+      "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
   end
 
   def container(number)
@@ -51,6 +43,14 @@ class Bottles
       'Go to the store and buy some more, '
     else
       "Take #{pronoun(number)} down and pass it around, "
+    end
+  end
+
+  def successor(number)
+    if number == 0
+      99
+    else
+      number - 1
     end
   end
 end
