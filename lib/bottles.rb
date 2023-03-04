b3101db3a0d780fb12e971295586c2a4f1d4a85c
Raluca Badoi <raluca.badoi@shopify.com>
2023-03-04 14:49:33 +0100

    Verse method uses BottleNumber factory

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,24 +8,14 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = bottle_number_for(number)
-    next_bottle_number = bottle_number_for(bottle_number.successor)
+    bottle_number = BottleNumber.for(number)
+    next_bottle_number = BottleNumber.for(bottle_number.successor)
+    # next_bottle_number = bottle_number.successor
 
     "#{bottle_number} of beer on the wall, ".capitalize +
       "#{bottle_number} of beer.\n" +
       "#{bottle_number.action}" +
       "#{next_bottle_number} of beer on the wall.\n"
-  end
-
-  def bottle_number_for(number)
-    case number
-    when 0
-      BottleNumber0
-    when 1
-      BottleNumber1
-    else
-      BottleNumber
-    end.new(number)
   end
 end
 
@@ -58,6 +48,17 @@ class BottleNumber
 
   def successor
     number - 1
+  end
+
+  def self.for(number)
+    case number
+      when 0
+        BottleNumber0
+      when 1
+        BottleNumber1
+      else
+        BottleNumber
+    end.new(number)
   end
 end
 
