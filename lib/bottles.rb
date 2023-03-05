0541c3e387153eefa97cc53c09115ecd3444fb43
Raluca Badoi <raluca.badoi@shopify.com>
2023-03-05 23:16:28 +0100

    Use VerseFake to test verse and verses

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -18,6 +18,12 @@ class CountdownSong
   end
 end
 
+class VerseFake
+  def self.lyrics(number)
+    "This is verse #{number}.\n"
+  end
+end
+
 class BottleVerse
   def self.lyrics(number)
     new(BottleNumber.for(number)).lyrics
