11d58e484c5665c4f83eebeb6d84e51da14ade67
Raluca Badoi <raluca.badoi@shopify.com>
2023-03-05 13:38:31 +0100

    Verse gets lyrics, new method of BottleVerse class

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,7 +8,7 @@ class Bottles
   end
 
   def verse(number)
-    BottleVerse.new(number).verse(number)
+    BottleVerse.new(number).lyrics
   end
 end
 
@@ -97,7 +97,7 @@ class BottleVerse
     @number = number
   end
 
-  def verse(number)
+  def lyrics
     bottle_number = BottleNumber.for(number)
 
     "#{bottle_number} of beer on the wall, ".capitalize +
