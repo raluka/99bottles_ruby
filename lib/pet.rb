5d5f072f8b1af9b7395f91b21d9815f12d3d0d59
Raluca Badoi <raluca.badoi@shopify.com>
2023-03-05 20:07:06 +0100

    The code in each method obeys Law of Demeter by sending messages only to direct collaborators

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,5 +1,24 @@
+class Friend
+  def durability_of_preferred_toy_of_pet
+    pet.durability_of_preferred_toy
+  end
+end
+
+class Pet
+  def durability_of_preferred_toy
+    preferred_toy.durability
+  end
+end
+
+class Toy
+  def durability
+    1.hour
+  end
+end
+
+# Foo now only sends messages to best_friend
 class Foo
   def durability_of_preferred_toy_of_best_friends_pet
-      best_friend.pet.preferred_toy.durability
-   end
+    best_friend.durability_of_preferred_toy_of_pet
+  end
 end
